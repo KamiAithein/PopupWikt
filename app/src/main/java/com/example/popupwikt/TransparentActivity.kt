cc5a0a6e486db05de65eb215e84a144733362dea
kamiaithein <k3nnethwkoepcke@gmail.com>
2020-06-14 18:37:58 -0500

    web browser display implemented, still no related results implementation

--- app/src/main/java/com/example/popupwikt/TransparentActivity.kt
+++ app/src/main/java/com/example/popupwikt/TransparentActivity.kt
@@ -3,45 +3,65 @@ package com.example.popupwikt
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Klaxon
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.readText
 import io.ktor.http.ContentType
+import io.ktor.http.isSuccess
 
 import kotlinx.android.synthetic.main.activity_transparent.*
 import kotlinx.coroutines.*
 import org.json.JSONObject
+import java.io.StringReader
+
+suspend fun httpRequest(url: String): HttpResponse {
+    val client = HttpClient()
+    val htmlContent = client.get<HttpResponse>(url)
+    client.close()
+    return htmlContent
+}
 
 class TransparentActivity : AppCompatActivity() {
-
-    private val wiktURL = "https://en.wikipedia.org/w/api.php"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val text = intent.extras?.get(Intent.EXTRA_PROCESS_TEXT).toString()
         setContentView(R.layout.activity_transparent)
-        val html = runBlocking {
-            queryWikt(text).readText()
+
+
+
+        //Try to get actual page
+        val response = runBlocking{
+            pageWikt(text)
         }
-        dialog(text, html)
+        if(response.status.isSuccess()){
+            val webview = findViewById<WebView>(R.id.webView)
+
+            runBlocking{
+                val respText = response.readText();
+            }
+            webview.webViewClient = WebViewClient()
+            webview.settings.javaScriptEnabled = true
+            webview.settings.domStorageEnabled = true
+            webview.overScrollMode = WebView.OVER_SCROLL_NEVER
+            webview.loadUrl(getPageUrl(text))
+
+        }
+        else{
+            runBlocking{
+                dialog(text, getSnippets(queryWikt(text).readText()).toString())
+            }
+        }
 
     }
-
-    private suspend fun httpRequest(url: String): HttpResponse{
-        val client = HttpClient()
-        val htmlContent = client.get<HttpResponse>(url)
-        client.close()
-        return htmlContent
-    }
-
-    private suspend fun queryWikt(query: String): HttpResponse {
-        return httpRequest("$wiktURL?action=query&list=search&srsearch=$query&format=json")
-    }
-
-
 
     private fun dialog(title: Int, message: Int){
         val builder: AlertDialog.Builder? = this.let{AlertDialog.Builder(it)}
@@ -61,4 +81,36 @@ class TransparentActivity : AppCompatActivity() {
         dialog?.show()
     }
 
+    companion object WiktAccess{
+
+        private val wiktURL = "https://en.wiktionary.org"
+        private val wiktAPI = "$wiktURL/w/api.php"
+        private val wiktPage = "$wiktURL/wiki"
+        private val klax = Klaxon()
+
+
+
+        private fun getPageUrl(page: String)
+                = "$wiktPage/$page"
+
+        private fun getQueryUrl(query: String)
+                = "$wiktAPI?action=query&list=search&srsearch=$query&format=json"
+
+        private suspend fun pageWikt(page: String): HttpResponse
+                = httpRequest(getPageUrl(page))
+
+        private suspend fun queryWikt(query: String): HttpResponse
+                = httpRequest(getQueryUrl(query))
+
+        /**
+         * Gets snippets from a wiktionary query
+         * @param rawQuery string query of wiktionary
+         * @return listOf(rawQuery.filter(snippets))
+         */
+        private fun getSnippets(rawQuery: String)
+                = ((klax.parseJsonObject(StringReader(rawQuery))
+                ["query"] as JsonObject)
+                ["search"] as JsonArray<*>)
+                .flatMap{ (it as JsonObject).entries.filter {entries -> entries.key == "snippet" }}
+    }
 }
