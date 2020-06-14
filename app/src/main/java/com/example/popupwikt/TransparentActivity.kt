29a60913b16a61f5579cce0a7885595edad29372
kamiaithein <k3nnethwkoepcke@gmail.com>
2020-06-13 23:42:15 -0500

    query works; all information needed gotten through ktor. Just need to format

--- app/src/main/java/com/example/popupwikt/TransparentActivity.kt
+++ app/src/main/java/com/example/popupwikt/TransparentActivity.kt
@@ -5,25 +5,43 @@ import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.readText
 import io.ktor.http.ContentType
 
 import kotlinx.android.synthetic.main.activity_transparent.*
+import kotlinx.coroutines.*
+import org.json.JSONObject
 
 class TransparentActivity : AppCompatActivity() {
 
+    private val wiktURL = "https://en.wikipedia.org/w/api.php"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val text = getIntent().getCharSequenceArrayExtra(Intent.EXTRA_PROCESS_TEXT)
+        val text = intent.extras?.get(Intent.EXTRA_PROCESS_TEXT).toString()
         setContentView(R.layout.activity_transparent)
-
-        dialog(R.string.dialog_fire_missiles, R.string.dialog_fire_missiles)
-    }
-
-    private fun ContentType.Application.module(){
-        routing{
-
+        val html = runBlocking {
+            queryWikt(text).readText()
         }
+        dialog(text, html)
+
     }
+
+    private suspend fun httpRequest(url: String): HttpResponse{
+        val client = HttpClient()
+        val htmlContent = client.get<HttpResponse>(url)
+        client.close()
+        return htmlContent
+    }
+
+    private suspend fun queryWikt(query: String): HttpResponse {
+        return httpRequest("$wiktURL?action=query&list=search&srsearch=$query&format=json")
+    }
+
+
 
     private fun dialog(title: Int, message: Int){
         val builder: AlertDialog.Builder? = this.let{AlertDialog.Builder(it)}
